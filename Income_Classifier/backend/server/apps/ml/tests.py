97d25f3346831639fb014471f7fd3bc20fb69012
Taha Hussein <taha.hussein@fci.helwan.edu.eg>
2020-01-18 20:09:22 +0200

    add predict view

--- Income_Classifier/backend/server/apps/ml/tests.py
+++ Income_Classifier/backend/server/apps/ml/tests.py
@@ -2,6 +2,8 @@ from django.test import TestCase
 import inspect
 from apps.ml.registry import MLRegistry
 from apps.ml.income_classifier.random_forest import RandomForestClassifier
+from django.test import TestCase
+from rest_framework.test import APIClient
 
 class MLTests(TestCase):
     def test_rf_algorithm(self):
@@ -44,3 +46,31 @@ class MLTests(TestCase):
                     algorithm_description, algorithm_code)
         # there should be one endpoint available
         self.assertEqual(len(registry.endpoints), 1)
+
+        
+class EndpointTests(TestCase):
+
+    def test_predict_view(self):
+        client = APIClient()
+        input_data = {
+            "age": 37,
+            "workclass": "Private",
+            "fnlwgt": 34146,
+            "education": "HS-grad",
+            "education-num": 9,
+            "marital-status": "Married-civ-spouse",
+            "occupation": "Craft-repair",
+            "relationship": "Husband",
+            "race": "White",
+            "sex": "Male",
+            "capital-gain": 0,
+            "capital-loss": 0,
+            "hours-per-week": 68,
+            "native-country": "United-States"
+        }
+        classifier_url = "/api/v1/income_classifier/predict"
+        response = client.post(classifier_url, input_data, format='json')
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.data["label"], "<=50K")
+        self.assertTrue("request_id" in response.data)
+        self.assertTrue("status" in response.data)
